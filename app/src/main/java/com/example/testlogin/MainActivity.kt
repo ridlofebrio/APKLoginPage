a0c7ea937ac3a5184690a031c83796e87acabb6c
Febrio <muhridlo111@gmail.com>
2023-10-16 20:06:24 +0700

    menyelesaikan desain dari loginpage

--- app/src/main/java/com/example/testlogin/MainActivity.kt
+++ app/src/main/java/com/example/testlogin/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.testlogin
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -10,10 +11,18 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
 import com.example.testlogin.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.button1
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnLoginListerner()
+}
+    private fun btnLoginListerner() {
+        button1.setOnClickListener(){
+            startActivities(arrayOf(Intent(this, Login::class.java)))
+        }
+    }
 
-}}
+}
